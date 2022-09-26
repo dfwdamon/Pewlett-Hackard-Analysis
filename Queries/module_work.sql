583e11ec91d4a97ad15439a9db817b476a6fbe3c
Damon <damonjphillips@gmail.com>
2022-09-25 22:12:08 -0500

    save

--- Queries/module_work.sql
+++ Queries/module_work.sql
@@ -39,6 +39,65 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
+
+--Join Retirement_info table with the Data_Emp emp_no column. --
+DROP TABLE retirement_info;
+
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+-- check the table
 SELECT * FROM retirement_info;
 
+-- redone with alias --
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date,
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
 
+	
+-- Joining retirement_info and dept_emp tables --
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp  -- second table
+ON retirement_info.emp_no = dept_emp.emp_no; -- ON two table and column names
+
+-- shorten with alias --
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp  -- second table
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- shortened --
+SELECT ri.emp_no,
+	ri.first_name,
+ri.last_name
+	de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+
+SELECT * FROM current_emp;
